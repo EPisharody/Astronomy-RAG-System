fdcb971cd7175009c04d22650996c5eb5d6187fc
EPisharody <esha.pisharody@gmail.com>
2026-01-23 15:51:32 -0500

    add gitignore and add code to create vector db

--- load_data.py
+++ load_data.py
@@ -1,7 +1,15 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from dotenv import load_dotenv
+import os
 
 FILE_PATH = "./data/general_astronomy_textbook.pdf"
+DB_PATH = "./chroma"
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Function to load documents from data folder
 def load_documents():
@@ -9,10 +17,16 @@ def load_documents():
     docs = loader.load()
     return docs
 
-# Function to split documents into chunks
+# Function to split documents into smaller chunks
 def split_documents(docs):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500, length_function=len, is_separator_regex=False)
     split_text = text_splitter.split_documents(docs)
     return split_text
 
-print(split_documents(load_documents())[5].page_content)
+def create_vector_db(split_docs):
+    vector_db = Chroma.from_documents(split_docs, OpenAIEmbeddings(), persist_directory=DB_PATH)
+    vector_db.persist()
+    print(F"Saved {len(split_docs)} chunks to {DB_PATH}.")
+
+split_text = split_documents(load_documents())
+create_vector_db(split_text)
